286e177cdbfecb6f543325c6abfbcf283b734f59
kskasa33 <Email>
2025-05-27 05:14:58 +0900

    bmiも定義

--- bmi.js
+++
@@ -1,6 +0,0 @@
-// 定数の宣言・値の代入
-let weight = 68;
-let height = 1.7;
-
-// コンソールへの出力
-console.log(weight/(height*height));
--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,7 @@
 // 定数の宣言・値の代入
 let weight = 68;
 let height = 1.7;
+let bmi
 
 // コンソールへの出力
-console.log(weight/(height*height));
+console.log(let=(weight/(height*height)));
